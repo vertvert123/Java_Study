1b1c017050922ed6807f655be7fc8e4ad952110d
vertvert123 <vertvert123@naver.com>
2021-12-20 18:01:43 +0900

    Operator1

--- Operator.java
+++ Operator.java
@@ -1,6 +1,25 @@
 public class Operator {
     public static void main(String[] args) {
-        
+        int a = 100;
+        int b = 200;
+
+        int result1 = a * b;
+        int result2 = a % b;
+
+        System.out.printf("result1 : %d\n",result1);
+        System.out.printf("result1 : %d\n",result2);
+        System.out.println("---------------------");
+
+        a++;
+        b *= 2;
+
+        System.out.printf("a++ : %d\n",a);
+        System.out.printf("b *= 2 : %d\n",b);
+        System.out.println("--------------");
+
+        System.out.printf("a++ : %d\n",a++);
+        System.out.printf("--a : %d",--a); 
+
     }
-    
+
 }
