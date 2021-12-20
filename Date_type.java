45a9f454b65cbd9ea28c17ee31b1014dd55462c4
vertvert123 <vertvert123@naver.com>
2021-12-20 17:29:55 +0900

    Data_Type

---
+++ Date_type.java
@@ -0,0 +1,16 @@
+public class Date_type {
+
+    public static void main(String[] args) {
+        int a =1;
+        float b =4.4f;// float 를 지정하기위해 실수값 뒤에 F/f를 붙여야한다.
+        double c = 2.567;
+        long d = 12341234L; //long을 지정하기위해 값 뒤에 L/l을 붙여햐한다.
+        boolean e = true;
+        char f = 'F';
+
+        String g = "hello world!"; // 문자열은 원시 자료형이아닌 클래스 타입이다.
+
+        
+    }
+
+}
