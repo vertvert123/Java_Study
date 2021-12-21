77493315aef14f58c24d9dc8eae68d238c898f87
vertvert123 <vertvert123@naver.com>
2021-12-21 23:57:34 +0900

    Loop

---
+++ Loop_statement.java
@@ -0,0 +1,24 @@
+public class Loop_statement {
+
+    public static void main(String[] args) {
+        int count = 12;
+        String people[] = {
+            "vert",
+            "vertvert",
+            "vertvert123"
+        };
+        for (int i = 0; i < 10; i++) { //for문 기초
+            System.out.println(i);
+        }
+        for (int i = 0; i < people.length; i++) { //for문으로 배열출력
+            System.out.println(people[i]);
+        }
+
+        while (count > 10) { //while 문
+            System.out.println("go");
+            count--;
+        }
+        System.out.println("exit");
+
+    }
+}
