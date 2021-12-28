f709cc66c3d4f7919b4dd9850f7966ab348810bb
vertvert123 <vertvert123@naver.com>
2021-12-28 19:54:57 +0900

    예외처리 실습1

---
+++ ExceptionTest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class ExceptionTest {
+    public static void main(String[] args) {
+        File file = new File("test.txt");
+        Scanner scan;
+
+        try {
+            scan = new Scanner(file);
+            while (scan.hasNext()) {
+                System.out.println(scan.next());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
