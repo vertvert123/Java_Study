aff2cae481d47e0cd34addd8432fef80b1746efb
vertvert123 <vertvert123@naver.com>
2022-01-10 23:46:13 +0900

    String 끝

--- StringTest/String_3.java
+++ StringTest/String_3.java
@@ -5,7 +5,17 @@ public class String_3 {
         String s1 = "Hello World";
         System.out.println(s1);
         System.out.printf("indexof('l'): %s\n",s1.indexOf('l'));
-        
+        System.out.printf("lastIndex('l'): %s\n", s1.lastIndexOf('l'));
+        System.out.printf("charAt(6): %s\n", s1.charAt(6));
+        System.out.printf("startWith(\"He\"): %s\n", s1.startsWith("He"));
+        System.out.printf("length(): %s\n", s1.length());
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("Hello");
+        sb.append(" World");
+        String str = sb.toString();
+        System.out.println(str);
+
     }
     
 
