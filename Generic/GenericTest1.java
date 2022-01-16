57066b509b2695aea0bba781ec489b2895f65b3d
vertvert123 <vertvert123@naver.com>
2022-01-17 02:45:50 +0900

    generic 기초

---
+++ Generic/GenericTest1.java
@@ -0,0 +1,17 @@
+package Generic;
+
+public class GenericTest1 {
+    public static void main(String[] args) {
+        Storage < String > storage1 = new Storage < > ();
+        storage1.setItem("MyItem");
+        System.out.println(storage1.getItem());
+
+        Storage<Integer> storage2 = new Storage<>();
+        storage2.setItem(20220117);
+
+        System.out.println(storage2.getItem());
+
+    }
+
+
+}
---
+++ Generic/Storage.java
@@ -0,0 +1,12 @@
+package Generic;
+
+public class Storage < T > {
+
+    T item;
+    public T getItem() {
+        return item;
+    }
+    public void setItem(T item){
+        this.item = item;
+    }
+}
