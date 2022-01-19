b0ce4814abef68b009f2b46e642930cd91ce39ff
vertvert123 <vertvert123@naver.com>
2022-01-19 23:01:05 +0900

    제네릭 응용

--- Generic/GeneriTest2.java
+++ Generic/GeneriTest2.java
@@ -1,7 +1,29 @@
 package Generic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GeneriTest2 {
-    public<T>List<Character> convert(Storage<T> storage){
-        ArrayList
+    public < T > List < Character > convert(Storage < T > storage) {
+        ArrayList < Character > list = new ArrayList < > ();
+        String s = String.valueOf(storage.getItem());
+        int size = s.length();
+        for (int i = 0; i < size; i++) {
+            list.add(s.charAt(i));
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Storage<String> s1 = new Storage<>();
+        s1.setItem("MyItem");
+
+        Storage<Integer> s2 = new Storage<>();
+        s2.setItem(20220119);
+
+        GeneriTest2 gt2 = new GeneriTest2();
+
+        System.out.println(gt2.convert(s1));
+        System.out.println(gt2.convert(s2));
     }
 }
