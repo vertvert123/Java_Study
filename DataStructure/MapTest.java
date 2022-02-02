1172c0bc53687626e508b7620b69325e2eda7300
vertvert123 <vertvert123@naver.com>
2022-02-02 23:45:17 +0900

    설명추가

--- DataStructure/MapTest.java
+++ DataStructure/MapTest.java
@@ -11,20 +11,20 @@ public class MapTest {
         map.put("2136861", "Samsung Galaxy");
         map.put("2136863", "Samsung Tablet");
 
-        System.out.println("1922877 : " + map.get("1922877"));
+        System.out.println("1922877 : " + map.get("1922877"));// Key를 이용해서 value값 찾기
 
         System.out.println("--------------------");
         for(Map.Entry<String,String> entry : map.entrySet()){
-            System.out.printf("%s:%s\n", entry.getKey(),entry.getValue());
+            System.out.printf("%s:%s\n", entry.getKey(),entry.getValue());// key 와 value 값 모두를 Entry 객체로 반환
         }
 
         System.out.println("--------------------");
         for(String s: map.keySet()){
-            System.out.printf("%s\n",s);
+            System.out.printf("%s\n",s);//key 요소만 Set 객체로 반환
         }
         System.out.println("--------------------");
         for(String s : map.values()){
-            System.out.printf("%s\n",s);
+            System.out.printf("%s\n",s);//value 요소만 Collection 타입으로 반환
         }
 
     }
